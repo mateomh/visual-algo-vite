0dc7fcfd57e134c51c9de94d7f5d0833c887c7ba
Mateo mojica <mmojicah.84@gmail.com>
2023-05-25 20:33:56 -0500

    add variables to the components to get the visualization factor on the board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,16 +3,22 @@ import Piece from './Piece';
 
 interface BoardProps {
   grid: string[][];
+  position: number[];
 }
 
 const Board:React.FC<BoardProps> = ({
-  grid
+  grid,
+  position:pos
 }) => {
 
   return (
     <div className="board">
-      {grid.map((row:string[]) => {
-        return row.map((item: string) => <Piece text={item} />)
+      {grid.map((row:string[], i) => {
+        return row.map((item: string, j) => {
+          const id = `${i}-${j}`
+          const highlight:boolean = id === `${pos[0]}-${pos[1]}`;
+          return<Piece key={`${i}-${j}`} id={id} text={item} highlight={highlight}/>
+        })
       })}
     </div>
   )
--- src/components/Piece.tsx
+++ src/components/Piece.tsx
@@ -2,13 +2,18 @@ import './Piece/styles.css';
 
 interface PieceProps {
   text: string;
+  id: string;
+  highlight: boolean;
 }
 
 const Piece:React.FC<PieceProps> = ({
-  text
+  text,
+  id,
+  highlight
 }) => {
+  const style = highlight ? {backgroundColor:'red'} : {}
   return (
-    <div className='piece'>
+    <div className='piece' id={id} style={style}>
       {text}
     </div>
   )
